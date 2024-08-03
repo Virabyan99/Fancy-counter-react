dbc774930f0f5ba3cda6bfefa977034ceace1f87
Andranik Virabyan <gmparstone99@gmail.com>
2024-08-03 01:18:48 -0800

    components added

--- src/components/ResetButton.jsx
+++ src/components/ResetButton.jsx
@@ -1,9 +1,16 @@
 import { ResetIcon } from '@radix-ui/react-icons'
 
-export default function ResetButtton() {
+export default function ResetButton({ setCount }) {
+  const handleClick = (event) => {
+    setCount(0);
+    event.currentTarget.blur()
+  }
   return (
-    <button className="reset-btn">
-      <ResetIcon className='reset-btn-icon' />
+    <button
+      onClick={handleClick}
+      className="reset-btn">
+      <ResetIcon className="reset-btn-icon" />
     </button>
   )
+
 }
